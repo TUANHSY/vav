fcab094ba4b0367302426538a5bb6211be568237
TUANHSY <1430737548@qq.com>
2019-12-11 14:44:25 +0800

    导入commons-io包

--- src/com/chinasoftware/aboutfile/FileTest4.java
+++ src/com/chinasoftware/aboutfile/FileTest4.java
@@ -1,8 +1,6 @@
 package com.chinasoftware.aboutfile;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
 
 /**
  * @author: DX
@@ -11,9 +9,22 @@ import java.io.ObjectOutputStream;
 public class FileTest4 {
     public static void main(String[] args) throws IOException {
 
+        //写
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
         objectOutputStream.writeUTF("俄代表处");
+        objectOutputStream.writeObject("你好");
+        objectOutputStream.writeObject(34567898763234567L);
+        objectOutputStream.writeObject(18d);
+        objectOutputStream.writeObject("你好测定");
+        objectOutputStream.writeObject(18f);
 
+        objectOutputStream.flush();
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+
+
+        //读
+        ObjectInputStream objectInputStream = new ObjectInputStream(new BufferedInputStream(new ByteArrayInputStream(bytes)));
+        objectInputStream.read();
     }
 }
